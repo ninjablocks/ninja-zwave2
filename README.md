ea6c67b12e86221cc1fa65680e41a40de47d4b06
☃ Elliot Shepherd <elliot@ninjablocks.com>
2014-04-25 11:31:12 +1000

    Version 0.0.2 - Works with aeon labs multi sensor

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-ninja-zwave
-==========
+ninja-zwave2
+============
 
 Requires an Aeon Labs Z-Stick S2 or Z-Stick Lite
+
+0.0.2
+ - Basic functionality
+ - Only tested with the Aeon Labs Multi Sensor (so far). There aren't any actuations yet.
--- index.js
+++ index.js
@@ -3,14 +3,11 @@
 var util = require('util');
 var stream = require('stream');
 var OZW = require('openzwave');
+var _ = require('underscore');
 
 util.inherits(Driver,stream);
 
-var commands = {
-  COMMAND_CLASS_SWITCH_BINARY: 0x25,
-  COMMAND_CLASS_SWITCH_MULTILEVEL: 0x26,
-  COMMAND_CLASS_SENSOR_BINARY: 0x30
-};
+var Commands = require('./lib/CommandClasses.js')
 
 function Driver(opts,app) {
   this.app = app;
@@ -18,7 +15,7 @@ function Driver(opts,app) {
 
   app.once('client::up', this.init.bind(this));
 
-  this.devices = {};
+  this.nodes = {};
 }
 
 Driver.prototype.init = function() {
@@ -30,11 +27,11 @@ Driver.prototype.init = function() {
 
   this.zwave = new OZW('/dev/cu.SLAB_USBtoUART', {
     logging: false,           // enable logging to OZW_Log.txt
-    consoleoutput: true,     // copy logging to the console
-    saveconfig: true,        // write an XML network layout
+    consoleoutput: false,     // copy logging to the console
+    saveconfig: false,        // write an XML network layout
     driverattempts: 3,        // try this many times before giving up
     pollinterval: 500,        // interval between polls in milliseconds
-    suppressrefresh: true,    // do not send updates if nothing changed
+    suppressrefresh: false,    // do not send updates if nothing changed
   });
 
   this.zwave.on('connected', function(){
@@ -43,39 +40,122 @@ Driver.prototype.init = function() {
 
   this.zwave.on('driver ready', function(homeId) {
     log.info('Scanning on home id', homeId);
-  });
+    this.homeId = homeId;
+  }.bind(this));
 
   this.zwave.on('node added', function(nodeId) {
-    log.info('Found new node', nodeId);
-  });
+    this.nodes[nodeId] = {commands:{}};
+  }.bind(this));
 
-  this.zwave.on('value added', this.onValueAdded.bind(this));
+  this.zwave.on('value added', function(nodeId, commandClass, value) {
+      var commands = this.nodes[nodeId].commands;
+      if (!commands[commandClass]) {
+        commands[commandClass] = [];
+      }
+      this.nodes[nodeId].commands[commandClass][value.index] = value;
+  }.bind(this));
+
+  this.zwave.on('value changed', function(nodeId, commandClass, v) {
+    console.log(v);
+      
+    var device = this.nodes[nodeId].commands[commandClass][v.index].device;
+    if (device) {
+      device.onValue(v);
+    } else if (v.genre == 'user' && this.nodes[nodeId].fullName) {
+      log.warn('Unused value', this.nodes[nodeId].fullName, commandClass, v.label, v);
+    }
+    
+  }.bind(this));
+
+  this.zwave.on('node ready', function(nodeId, nodeInfo) {
+    var node = this.nodes[nodeId];
+
+    if (node.fullName) {
+      return; // We've seen this one before
+    }
+
+    node.id = nodeId;
+    _.extend(node, nodeInfo);
+
+    node.fullName = (node.name? node.name + ' - ': '') + node.product + ' by ' + node.manufacturer;
+
+    log.info('Found node -', node.fullName);
+    
+    this.onNodeReady(node);
+  }.bind(this));
 
   this.zwave.connect();
   
 };
 
-Driver.prototype.onValueChanged = function(nodeId, commandClass, value) {
+
+Driver.prototype.onNodeReady = function(node) {
+
   var log = this.app.log;
-  log.debug('Value changed nodeId:', nodeId, 'commandClass:', commandClass, 'value:', value);
+
+ 
+
+  Object.keys(node.commands).forEach(function(commandId) {
+
+    switch (commandId) {
+      case Commands.SWITCH_BINARY:
+      case Commands.SWITCH_MULTILEVEL:
+      case Commands.SWITCH_MULTILEVEL_V2:
+        this.zwave.enablePoll(node.id, commandId);
+        break;
+    }
+
+    node.commands[commandId].forEach(function(value) {
+      if (!value) return;
+
+      var device = new Device();
+      device.G = 'zwave' +  this.homeId + node.id + commandId + value.index;
+      device.name = value.label + ' - ' + node.fullName;
+      
+      if (value.label.match(/temperature/i)) {
+        device.V = 0;
+        device.D = 9;
+        device.onValue = function(value) {
+          this.emit('data', (value.value - 32) * 5 / 9.0);
+        };
+      } else if (value.label.match(/humidity/i)) {
+        device.V = 0;
+        device.D = 8;
+        device.onValue = function(value) {
+          this.emit('data', value.value)
+        };
+      } else if (value.label.match(/battery/i) && value.type == 'byte') {
+        device.V = 0;
+        device.D = 8; // Use humidity for now
+        device.onValue = function(value) {
+          this.emit('data', value.value)
+        };
+      } else if (value.label.match(/luminance/i)) {
+        device.V = 0;
+        device.D = 2000;
+        device.onValue = function(value) {
+          this.emit('data', value.value)
+        };
+      } else if (value.label.match(/sensor/i) && value.type == 'bool') {
+        device.V = 0;
+        device.D = 7;
+        device.onValue = function(value) {
+          this.emit('data', value.value?'0':'1')
+        };
+      }
+
+      if (device.D) {
+        value.device = device;
+        this.emit('register', device);
+      }
+      
+    }.bind(this));
+  }.bind(this));
+
 };
 
-Driver.prototype.onValueAdded = function(nodeId, commandClass, value) {
-  var log = this.app.log;
-  //log.debug('Found nodeId:', nodeId, 'commandClass:', commandClass, 'value:', value);
+function Device() {
+}
+util.inherits(Device, stream);
 
-  var id = nodeId
-
-  if (commandClass === commands.COMMAND_CLASS_SWITCH_BINARY || commands.COMMAND_CLASS_SWITCH_MULTILEVEL) {
-    this.zwave.enablePoll(nodeId, commandClass);
-  }
-
-  if (commandClass === commands.COMMAND_CLASS_SENSOR_BINARY) {
-    log.debug('Found a sensor');
-    log.debug('Found nodeId:', nodeId, 'commandClass:', commandClass, 'value:', value);
-  }
-
-};
-
-// Export it
 module.exports = Driver;
